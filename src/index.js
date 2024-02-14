52dd05dc5534bdf77e5e9a2c6faad630dbb04830
Dawit Bireda <davebireda2394@gmail.com>
2024-02-14 16:38:46 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Bootstrap CSS & JS imports
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Components imports
+import CreateBook from "./components/CreateBook";
+import ShowBookList from "./components/ShowBookList";
+import ShowBookDetails from "./components/ShowBookDetails";
+import UpdateBookInfo from "./components/UpdateBookInfo";
+
+// Routes
+const router = createBrowserRouter([
+  { path: "/", element: <ShowBookList /> },
+  { path: "/create-book", element: <CreateBook /> },
+  { path: "/show-book/:id", element: <ShowBookDetails /> },
+  { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
